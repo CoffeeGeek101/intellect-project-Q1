a3a29dee1ffa4b7a947952680a8522ffe3f4df62
CoffeeGeek101 <workwithshoumya@gmail.com>
2025-05-31 19:59:12 +0530

    add : utility for mock data

---
+++ src/util/feedback_data.ts
@@ -0,0 +1,28 @@
+import terrible_emoji from '../assets/bad-review.png'
+import bad_emoji from '../assets/disappointed.png'
+import alright_emoji from '../assets/neutral-face.png'
+import pretty_good_emoji from '../assets/smiley-icon.png'
+import fantastic_emoji from '../assets/grinning-face-with-big-eyes-emoji-icon.png'
+
+export const feedback_data = [
+    {
+        icon : terrible_emoji,
+        title : 'Terrible'
+    },
+    {
+        icon : bad_emoji,
+        title : 'Bad'
+    },
+    {
+        icon : alright_emoji,
+        title : 'Alright'
+    },
+    {
+        icon : pretty_good_emoji,
+        title : 'Pretty Good'
+    },
+    {
+        icon : fantastic_emoji,
+        title : 'Fantastic'
+    }
+]
